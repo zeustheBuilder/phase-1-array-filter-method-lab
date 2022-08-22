7642f308ce1bd4c9d66625812bb819d3d4f33375
Brian <bgitau35@gmail.com>
2022-08-22 14:17:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching (drivers, taxi) {
+    return drivers.filter (
+        (possibleMatch) => possibleMatch.toLowerCase() === taxi.toLowerCase()
+    );
+}
+
+function fuzzyMatch (drivers, oneTaxi) {
+    return drivers.filter(
+        (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(oneTaxi.toLowerCase()) === 0
+    );
+}
+
+function matchName (drivers, matchName) {
+    return drivers.filter((record) => record.name === matchName);
+} 
